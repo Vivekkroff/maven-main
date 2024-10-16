32b26fab9f99a58f0dfafbc418130a3d5b4b3af5
root <root@ip-172-31-1-225.ec2.internal>
2024-10-16 13:05:13 +0000

    maven

---
+++ homepage/src/main/java/Twitter/sampleApp.java
@@ -0,0 +1,14 @@
+package Twitter;
+
+/**
+ * Hello world!
+ *
+ */
+public class sampleApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
